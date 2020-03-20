0d23fdfa589f7fc3a9c008bc88001a45f1063c01
Pichu <aristi.esteban@outlook.com>
2020-03-20 20:02:11 -0300

    Parte E

--- src/com/company/Curso.java
+++ src/com/company/Curso.java
@@ -1,9 +1,23 @@
 package com.company;
 
+import java.util.List;
+
 public class Curso {
 
     private String nombre;
     private Integer codigoCurso;
+    private ProfesorTitular unProfesorTitular;
+    private ProfesorAdjunto unProfesorAdjunto;
+    private List<Alumno> listaDeAlumnos;
+
+
+    public Curso(String nombre, Integer codigoCurso, ProfesorTitular unProfesorTitular, ProfesorAdjunto unProfesorAdjunto, List<Alumno> listaDeAlumnos) {
+        this.nombre = nombre;
+        this.codigoCurso = codigoCurso;
+        this.unProfesorTitular = unProfesorTitular;
+        this.unProfesorAdjunto = unProfesorAdjunto;
+        this.listaDeAlumnos = listaDeAlumnos;
+    }
 
     public String getNombre() {
         return nombre;
@@ -19,6 +33,30 @@ public class Curso {
 
     public void setCodigoCurso(Integer codigoCurso) {
         this.codigoCurso = codigoCurso;
+    }
+
+    public ProfesorTitular getUnProfesorTitular() {
+        return unProfesorTitular;
+    }
+
+    public void setUnProfesorTitular(ProfesorTitular unProfesorTitular) {
+        this.unProfesorTitular = unProfesorTitular;
+    }
+
+    public ProfesorAdjunto getUnProfesorAdjunto() {
+        return unProfesorAdjunto;
+    }
+
+    public void setUnProfesorAdjunto(ProfesorAdjunto unProfesorAdjunto) {
+        this.unProfesorAdjunto = unProfesorAdjunto;
+    }
+
+    public List<Alumno> getListaDeAlumnos() {
+        return listaDeAlumnos;
+    }
+
+    public void setListaDeAlumnos(List<Alumno> listaDeAlumnos) {
+        this.listaDeAlumnos = listaDeAlumnos;
     }
 
     @Override
